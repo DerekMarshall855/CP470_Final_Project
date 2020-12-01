70dffabf4b885c1996bddd4c7fa8a829f775add8
DerekMarshall855 <derekmarshall855@gmail.com>
2020-12-01 12:30:57 -0500

    Added Level Select, needs a lot of work. Play now opens level select and selecting level now opens GameActivity

--- app/src/main/java/com/example/cp470_final_project/MainActivity.java
+++ app/src/main/java/com/example/cp470_final_project/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                Intent intent = new Intent(MainActivity.this, LevelSelect.class);
                 startActivity(intent);
             }
         });
