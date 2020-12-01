a5201eaf44ad2d7f19c621e9dde7caef5f692479
beccascarlett <rebecca.scarlett@hotmail.com>
2020-12-01 16:45:00 -0500

    changed the main background and took away title in the toolbar

--- app/src/main/java/com/example/cp470_final_project/MainActivity.java
+++ app/src/main/java/com/example/cp470_final_project/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
 
